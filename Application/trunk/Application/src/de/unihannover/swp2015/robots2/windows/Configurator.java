94f80227bcbbe33a9437d76b61f3fcd701d67b1d
Tim Ebbeke <tim06tr@gmail.com>
2015-11-09 21:53:17 +0000

    Added TabPane. Still trying to get the font right.
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@118 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Application/trunk/Application/src/de/unihannover/swp2015/robots2/windows/Configurator.java
+++ Application/trunk/Application/src/de/unihannover/swp2015/robots2/windows/Configurator.java
@@ -2,13 +2,19 @@ package de.unihannover.swp2015.robots2.windows;
 
 import java.net.URL;
 
+import org.apache.pivot.beans.BXML;
 import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.Component;
+import org.apache.pivot.wtk.TabPane;
 import org.apache.pivot.wtk.Window;
+import org.apache.pivot.wtk.skin.terra.TerraTabPaneSkin;
 
 public class Configurator extends Window implements Bindable
 {
+	@BXML private TabPane tabs;
+	
 	@Override
 	public void initialize(Map<String, Object> namespace, URL location, Resources resources) {
 		
