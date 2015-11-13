b130ede7126598a5cd6fbf282a2a3626dd3d5164
Michael Thies <mail@mhthies.de>
2015-11-13 19:49:51 +0000

    Fix controller.externalInterfaces.IVisualization
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@163 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Protocol/trunk/Protocol/src/de/unihannover/swp2015/robots2/controller/externalInterfaces/IVisualization.java
+++ Protocol/trunk/Protocol/src/de/unihannover/swp2015/robots2/controller/externalInterfaces/IVisualization.java
@@ -2,4 +2,8 @@ package de.unihannover.swp2015.robots2.controller.externalInterfaces;
 
 public interface IVisualization {
 
+	public void setSettings(String settings);
+	
+	public String getSettings();
+
 }
