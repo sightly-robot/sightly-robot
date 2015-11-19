934c727e84b305ac03c6179be8d8ae571dd80733
Michael Thies <mail@mhthies.de>
2015-11-19 19:25:25 +0000

    Fix MqttController and add Javadocs (2)
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@182 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Protocol/trunk/Protocol/src/de/unihannover/swp2015/robots2/controller/mqtt/MqttReceiver.java
+++ Protocol/trunk/Protocol/src/de/unihannover/swp2015/robots2/controller/mqtt/MqttReceiver.java
@@ -3,28 +3,32 @@ package de.unihannover.swp2015.robots2.controller.mqtt;
 import java.util.concurrent.BlockingQueue;
 
 /**
- * Worker thread for receiving MQTT messages. Sequentially reads the messages from the given BlockingQueue and calls the callback method of the given message handler.  
+ * Worker thread for receiving MQTT messages. Sequentially reads the messages
+ * from the given BlockingQueue and calls the callback method of the given
+ * message handler.
  * 
  * @author Michael Thies
  */
 public class MqttReceiver implements Runnable {
 	private final BlockingQueue<MqttFullMessage> queue;
 	private final IMqttMessageHandler handler;
-	
-	public MqttReceiver (BlockingQueue<MqttFullMessage> queue, IMqttMessageHandler handler) {
+
+	public MqttReceiver(BlockingQueue<MqttFullMessage> queue,
+			IMqttMessageHandler handler) {
 		this.queue = queue;
 		this.handler = handler;
 	}
-	
+
 	@Override
 	public void run() {
 		while (true) {
 			try {
 				MqttFullMessage message = this.queue.take();
-				handler.handleMqttMessage(message.getTopic(), message.getMessage().getPayload().toString());
+				handler.handleMqttMessage(message.getTopic(), message
+						.getMessage().getPayload().toString());
 			} catch (InterruptedException e) {
 				e.printStackTrace();
-			}	
+			}
 		}
 	}
 }
