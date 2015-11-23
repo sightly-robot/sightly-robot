421e3a54947a8cfe741d5dbee733dabb44084304
Michael Thies <mail@mhthies.de>
2015-11-23 12:00:52 +0000

    Improve MQTT Topic parser
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@211 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Protocol/trunk/Protocol/src/de/unihannover/swp2015/robots2/controller/mqtt/MqttTopic.java
+++ Protocol/trunk/Protocol/src/de/unihannover/swp2015/robots2/controller/mqtt/MqttTopic.java
@@ -1,9 +1,5 @@
 package de.unihannover.swp2015.robots2.controller.mqtt;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 /**
  * 
  * @author Patrick Kawczynski
@@ -11,63 +7,51 @@ import java.util.Set;
  */
 public enum MqttTopic {
 
-	ROBOT_DISCOVER("robot/discover"), 
-	ROBOT_NEW("robot/new"), 
-	ROBOT_TYPE("robot/type/+"), 
-	ROBOT_POSITION("robot/position/+"), 
-	ROBOT_SETPOSITION("robot/setposition/+"), 
-	ROBOT_VIRTUALSPEED("robot/virtualspeed"), 
-	MAP_WALLS("map/walls"), 
-	MAP_FOOD("map/food"), 
-	FIELD_FOOD("map/food/+"), 
-	FIELD_OCCUPIED_LOCK("map/occupied/lock/+"), 
-	FIELD_OCCUPIED_SET("map/occupied/set/+"), 
-	FIELD_OCCUPIED_RELEASE("map/occupied/release/+"), 
-	CONTROL_STATE("control/state"), 
-	EVENT_ERROR_SERVER_CONNECTION("event/error/server/connection"), 
-	EVENT_ERROR_ROBOT_CONNECTION("event/error/robot/+/connection"), 
-	EVENT_ERROR_ROBOT_ROBOTICS("event/error/robot/+/robotics");
+	ROBOT_DISCOVER("robot/discover"), ROBOT_NEW("robot/new"), ROBOT_TYPE(
+			"robot/type/+"), ROBOT_POSITION("robot/position/+"), ROBOT_SETPOSITION(
+			"robot/setposition/+"), ROBOT_VIRTUALSPEED("robot/virtualspeed"), MAP_WALLS(
+			"map/walls"), MAP_FOOD("map/food"), FIELD_FOOD("map/food/+"), FIELD_OCCUPIED_LOCK(
+			"map/occupied/lock/+"), FIELD_OCCUPIED_SET("map/occupied/set/+"), FIELD_OCCUPIED_RELEASE(
+			"map/occupied/release/+"), CONTROL_STATE("control/state"), EVENT_ERROR_SERVER_CONNECTION(
+			"event/error/server/connection"), EVENT_ERROR_ROBOT_CONNECTION(
+			"event/error/robot/+/connection"), EVENT_ERROR_ROBOT_ROBOTICS(
+			"event/error/robot/+/robotics");
 
 	private final String topic;
-	// Reverse-lookup map for getting a MqttTopic-Object from an Topic-String
-	private static final Map<String, MqttTopic> lookup = new HashMap<String, MqttTopic>();
 
 	/**
-	 * Creates the reverse-lookup map
-	 */
-	static {
-		for (MqttTopic topic : MqttTopic.values()) {
-			lookup.put(topic.toString(), topic);
-		}
-	}
-
-	/**
-	 * Returns the MqttTopic-Object by the given topic. This method can also
-	 * find topics, which contains keys instead of a "+" or a "#".
+	 * Returns the MqttTopic-Object by the given topic string. This method is
+	 * able to expand mqtt wildcards (+, #) used in stored topics.
 	 * 
-	 * @param topic
+	 * @param input
 	 *            The topic
 	 * @return A mqtt-topic object or null, if the given topic does not exists
 	 *         in this enumeration.
 	 */
-	public static MqttTopic getBy(String topic) {
+	public static MqttTopic getBy(String input) {
 
-		String[] topicParts = topic.split("/");
-
-		Set<String> topics = lookup.keySet();
-		for (String currentTopic : topics) {
-			String[] currentTopicParts = currentTopic.split("/");
-
-			for (int i = 0; i < currentTopicParts.length; i++) {
-				if (currentTopicParts[i].equals("+") || currentTopicParts[i].equals(topicParts[i])) {
-					if (i == currentTopicParts.length - 1) {
-						return lookup.get(currentTopic);
-					} else {
-						continue;
-					}
+		for (MqttTopic topic : MqttTopic.values()) {
+			String expression = topic.toString();
+			int i = 0, j = 0;
+			while (i < input.length() && j < expression.length()) {
+				if (input.charAt(i) == expression.charAt(j)) {
+					i++;
+					j++;
+				} else if (expression.charAt(j) == '#') {
+					// Wildcard for any following string => accept
+					return topic;
+				} else if (expression.charAt(j) == '+') {
+					// Wildcard for any string before next slash
+					while (input.charAt(i) != '/' && i < input.length())
+						i++;
 				} else {
+					// Strings to not match
 					break;
 				}
+			}
+			if (i == input.length() && j == expression.length()) {
+				// Exact match
+				return topic;
 			}
 		}
 
