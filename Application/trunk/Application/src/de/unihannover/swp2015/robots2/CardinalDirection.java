1adeae48cf9cf6b635c3f59b13730f189fa951e5
Tim Ebbeke <tim06tr@gmail.com>
2015-11-24 21:18:11 +0000

    Added angle functionality to enum.
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@235 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Application/trunk/Application/src/de/unihannover/swp2015/robots2/CardinalDirection.java
+++ Application/trunk/Application/src/de/unihannover/swp2015/robots2/CardinalDirection.java
@@ -1,8 +1,22 @@
 package de.unihannover.swp2015.robots2;
 
 public enum CardinalDirection {
-	NORTH,
-	WEST,
-	SOUTH,
-	EAST
+	NORTH(0),
+	EAST(90),
+	SOUTH(180),
+	WEST(270);
+	
+	private final int angle;
+	CardinalDirection(int angle) {
+		this.angle = angle;
+	}
+	
+	public int getAngle() {
+		return this.angle;
+	}
+	
+	public int getAngleDifference(CardinalDirection otherDirection) {
+		return Math.max(otherDirection.getAngle(), getAngle()) - 
+			   Math.min(otherDirection.getAngle(), getAngle());
+	}
 }
