419fb6dcaa3d2c4d242671ff504e1a054f3a25ef
Philip Naumann <pnaumann@19a0f6ba-50c4-4f9a-9110-d500b7697500>
2015-11-28 19:52:34 +0000

    Added invalid stage exception for AI graph
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@295 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Ai/trunk/Ai/src/de/unihannover/swp2015/robots2/graph/AIGraph.java
+++ Ai/trunk/Ai/src/de/unihannover/swp2015/robots2/graph/AIGraph.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 import de.unihannover.swp2015.robots2.core.Robot;
+import de.unihannover.swp2015.robots2.exceptions.InvalidStageException;
 import de.unihannover.swp2015.robots2.model.interfaces.IField;
 import de.unihannover.swp2015.robots2.model.interfaces.IPosition;
 import de.unihannover.swp2015.robots2.model.interfaces.IPosition.Orientation;
@@ -29,54 +30,66 @@ public class AIGraph {
 		this.dimY = y;
 	}
 
-	public AIGraph(IStage map) {
-		this.loadFromMap(map);
+	public AIGraph(IStage stage) throws InvalidStageException {
+		this.loadFromStage(stage);
 	}
 
 	/**
 	 * Loads map data into Graph object
 	 * 
-	 * @param map
+	 * @param stage
 	 *            IStage data object
+	 * @throws InvalidStageException
 	 */
-	public void loadFromMap(IStage map) {
-		int width = map.getWidth();
-		int height = map.getHeight();
+	public void loadFromStage(IStage stage) throws InvalidStageException {
+		/*
+		 * Handle exceptions for faulty stages
+		 */
+		if (stage == null) {
+			throw new InvalidStageException("Stage is empty");
+		} else if (stage.getWidth() < 1) {
+			throw new InvalidStageException("Width of stage is too small");
+		} else if (stage.getHeight() < 1) {
+			throw new InvalidStageException("Height of stage is too small");
+		}
+
+		int width = stage.getWidth();
+		int height = stage.getHeight();
 
 		this.nodes = new Node[width][height];
 		this.dimX = width;
 		this.dimY = height;
 
 		/*
-		 * Init graph from map data
+		 * Init graph from stage data
 		 */
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
-				IField field = map.getField(i, j);
+				IField field = stage.getField(i, j);
 				/*
 				 * Check if node already exists and eventually create new one
 				 */
 				Node source = this.nodes[i][j] == null ? new Node(field) : this.nodes[i][j];
 				if (i > 0 && !field.isWall(Orientation.WEST)) {
-					Node target = this.nodes[i - 1][j] == null ? new Node(map.getField(i - 1, j))
+					Node target = this.nodes[i - 1][j] == null ? new Node(stage.getField(i - 1, j))
 							: this.nodes[i - 1][j];
 					Edge edge = new Edge(source, target, Orientation.WEST);
 					source.getNeighbors().add(edge);
 				}
 				if (i < this.dimX && !field.isWall(Orientation.EAST)) {
-					Node target = this.nodes[i + 1][j] == null ? new Node(map.getField(i + 1, j))
+					Node target = this.nodes[i + 1][j] == null ? new Node(stage.getField(i + 1, j))
 							: this.nodes[i + 1][j];
 					Edge edge = new Edge(source, target, Orientation.EAST);
 					source.getNeighbors().add(edge);
 				}
 				if (j > 0 && !field.isWall(Orientation.NORTH)) {
-					Node target = this.nodes[i][j - 1] == null ? new Node(map.getField(i, j - 1))
+					Node target = this.nodes[i][j - 1] == null ? new Node(stage.getField(i, j - 1))
 							: this.nodes[i][j - 1];
 					Edge edge = new Edge(source, target, Orientation.NORTH);
 					source.getNeighbors().add(edge);
 				}
 				if (j < this.dimY && !field.isWall(Orientation.SOUTH)) {
-					Node target = this.nodes[i][j + 1] == null ? new Node(map.getField(i, j + 1))
+					Node target = this.nodes[i][j + 1] == null ? new Node(stage.getField(i, j + 1))
 							: this.nodes[i][j + 1];
 					Edge edge = new Edge(source, target, Orientation.SOUTH);
 					source.getNeighbors().add(edge);
@@ -87,63 +100,67 @@ public class AIGraph {
 		/*
 		 * Set our and other robots positions
 		 */
-		for (IPosition pos : map.getStartPositions()) {
+		for (IPosition pos : stage.getStartPositions()) {
 			Node posNode = this.nodes[pos.getX()][pos.getY()];
-			IField field = map.getField(pos.getX(), pos.getY());
+			IField field = stage.getField(pos.getX(), pos.getY());
 			String id = field.getLockedBy();
 			Robot robot = new Robot(id, posNode, pos.getOrientation());
 			posNode.setRobot(robot);
 		}
 	}
-	
-	//not returning backward in increment 1
+
+	// not returning backward in increment 1
 	/**
-	 * This method returns a random valid Orientation, which the robot is supposed to 
-	 * move in next.
+	 * This method returns a random valid Orientation, which the robot is
+	 * supposed to move in next.
 	 * 
-	 * @return	Orientation, the robot should move in next
+	 * @return Orientation, the robot should move in next
 	 */
-	//TODO: Exception if there is no valid Orientation
+	// TODO: Exception if there is no valid Orientation
 	public Orientation getRandomOrientation() {
 		List<Orientation> available = new ArrayList<Orientation>();
 		Orientation curr = this.myself.getOrientation();
 		Node myPos = getMyPosition();
-		
-		switch(curr) {
-			case NORTH:	if(!myPos.isWall(Orientation.WEST))
-							available.add(Orientation.WEST);
-						if(!myPos.isWall(Orientation.NORTH))
-							available.add(Orientation.NORTH);
-						if(!myPos.isWall(Orientation.EAST))
-							available.add(Orientation.EAST);
-						break;
-			case EAST:	if(!myPos.isWall(Orientation.NORTH))
-							available.add(Orientation.NORTH);
-						if(!myPos.isWall(Orientation.EAST))
-							available.add(Orientation.EAST);
-						if(!myPos.isWall(Orientation.SOUTH))
-							available.add(Orientation.SOUTH);
-						break;
-			case SOUTH:	if(!myPos.isWall(Orientation.EAST))
-							available.add(Orientation.EAST);
-						if(!myPos.isWall(Orientation.SOUTH))
-							available.add(Orientation.SOUTH);
-						if(!myPos.isWall(Orientation.WEST))
-							available.add(Orientation.WEST);
-						break;
-			case WEST:	if(!myPos.isWall(Orientation.SOUTH))
-							available.add(Orientation.SOUTH);
-						if(!myPos.isWall(Orientation.WEST))
-							available.add(Orientation.WEST);
-						if(!myPos.isWall(Orientation.NORTH))
-							available.add(Orientation.NORTH);
-						break;
+
+		switch (curr) {
+		case NORTH:
+			if (!myPos.isWall(Orientation.WEST))
+				available.add(Orientation.WEST);
+			if (!myPos.isWall(Orientation.NORTH))
+				available.add(Orientation.NORTH);
+			if (!myPos.isWall(Orientation.EAST))
+				available.add(Orientation.EAST);
+			break;
+		case EAST:
+			if (!myPos.isWall(Orientation.NORTH))
+				available.add(Orientation.NORTH);
+			if (!myPos.isWall(Orientation.EAST))
+				available.add(Orientation.EAST);
+			if (!myPos.isWall(Orientation.SOUTH))
+				available.add(Orientation.SOUTH);
+			break;
+		case SOUTH:
+			if (!myPos.isWall(Orientation.EAST))
+				available.add(Orientation.EAST);
+			if (!myPos.isWall(Orientation.SOUTH))
+				available.add(Orientation.SOUTH);
+			if (!myPos.isWall(Orientation.WEST))
+				available.add(Orientation.WEST);
+			break;
+		case WEST:
+			if (!myPos.isWall(Orientation.SOUTH))
+				available.add(Orientation.SOUTH);
+			if (!myPos.isWall(Orientation.WEST))
+				available.add(Orientation.WEST);
+			if (!myPos.isWall(Orientation.NORTH))
+				available.add(Orientation.NORTH);
+			break;
 		}
-		
+
 		Collections.shuffle(available);
 		return available.get(0);
 	}
-	
+
 	public void setStartPositions(IStage map) {
 
 	}
