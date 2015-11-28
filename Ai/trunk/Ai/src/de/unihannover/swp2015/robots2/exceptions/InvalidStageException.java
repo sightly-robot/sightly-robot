8b0b762aeadeb3c3deab296237372d302c11e9af
Philip Naumann <pnaumann@19a0f6ba-50c4-4f9a-9110-d500b7697500>
2015-11-28 19:53:16 +0000

    Add package
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@296 8fccc7c9-0077-40ea-a901-f51b848cf750

---
+++ Ai/trunk/Ai/src/de/unihannover/swp2015/robots2/exceptions/InvalidStageException.java
@@ -0,0 +1,12 @@
+package de.unihannover.swp2015.robots2.exceptions;
+
+public class InvalidStageException extends Exception {
+
+	public InvalidStageException() {
+	}
+
+	public InvalidStageException(String msg) {
+		super(msg);
+	}
+
+}
