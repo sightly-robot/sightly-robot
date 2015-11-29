7d06c4e3db1f07bb719b8e2a4a402355dc052246
Timon Barlag <timon.barlag@gmx.de>
2015-11-29 16:38:57 +0000

    junit test
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@309 8fccc7c9-0077-40ea-a901-f51b848cf750

---
+++ Ai/trunk/Ai/src/de/unihannover/swp2015/robots2/tests/TestTest.java
@@ -0,0 +1,14 @@
+package de.unihannover.swp2015.robots2.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
