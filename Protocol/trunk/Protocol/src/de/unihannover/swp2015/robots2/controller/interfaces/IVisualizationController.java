24aa0b99972e906181ff34b5ebd1a2d5949e071f
Michael Thies <mail@mhthies.de>
2015-11-30 15:29:39 +0000

    Implement VisualizationMainController
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@332 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Protocol/trunk/Protocol/src/de/unihannover/swp2015/robots2/controller/interfaces/IVisualizationController.java
+++ Protocol/trunk/Protocol/src/de/unihannover/swp2015/robots2/controller/interfaces/IVisualizationController.java
@@ -1,5 +1,17 @@
 package de.unihannover.swp2015.robots2.controller.interfaces;
 
+import de.unihannover.swp2015.robots2.controller.externalInterfaces.IVisualization;
+import de.unihannover.swp2015.robots2.controller.externalInterfaces.IVisualizationControl;
+
 public interface IVisualizationController extends IController {
+
+	/**
+	 * Register an IVisualization that will receive, store and use the settings
+	 * broadcasted with an {@link IVisualizationControl}.
+	 * 
+	 * @param control
+	 *            The visualization component that stores settings
+	 */
+	public void registerVisualization(IVisualization visualization);
 
 }
--- Protocol/trunk/Protocol/src/de/unihannover/swp2015/robots2/controller/main/VisualizationMainController.java
+++ Protocol/trunk/Protocol/src/de/unihannover/swp2015/robots2/controller/main/VisualizationMainController.java
@@ -8,15 +8,20 @@ import java.util.UUID;
 
 import org.eclipse.paho.client.mqttv3.MqttException;
 
+import de.unihannover.swp2015.robots2.controller.externalInterfaces.IVisualization;
 import de.unihannover.swp2015.robots2.controller.interfaces.IVisualizationController;
 import de.unihannover.swp2015.robots2.controller.mqtt.MqttController;
+import de.unihannover.swp2015.robots2.controller.mqtt.MqttTopic;
+import de.unihannover.swp2015.robots2.model.interfaces.IEvent.UpdateType;
+import de.unihannover.swp2015.robots2.model.writeableInterfaces.IRobotWriteable;
 
 /**
  * 
  * @author Michael Thies
  */
-public class VisualizationMainController extends AbstractMainController
-		implements IVisualizationController {
+public class VisualizationMainController extends AbstractMainController implements IVisualizationController {
+
+	private IVisualization visualization;
 
 	public VisualizationMainController() {
 		super();
@@ -29,11 +34,13 @@ public class VisualizationMainController extends AbstractMainController
 		if (this.mqttController == null) {
 			String clientId = "visu_" + UUID.randomUUID().toString();
 			// TODO subscription list
-			String[] subscribeTopics = {};
+			String[] subscribeTopics = { "robot/#", "extension/2/robot/#",
+					"map/walls", "map/food", "map/food/+",
+					"map/occupied/+/set", "event/error/robot/#",
+					"extension/2/settings/visualization/#", "control/state"};
 
 			try {
-				this.mqttController = new MqttController(brokerUrl, clientId,
-						this, Arrays.asList(subscribeTopics));
+				this.mqttController = new MqttController(brokerUrl, clientId, this, Arrays.asList(subscribeTopics));
 				return true;
 			} catch (MqttException e) {
 				e.printStackTrace();
@@ -44,9 +51,80 @@ public class VisualizationMainController extends AbstractMainController
 		}
 	}
 
+	@Override
 	public void handleMqttMessage(String topic, String message) {
-		// TODO Auto-generated method stub
+		MqttTopic mqtttopic = MqttTopic.getBy(topic);
+		if (mqtttopic == null)
+			return;
 
+		String key = mqtttopic.getKey(topic);
+
+		switch (mqtttopic) {
+
+		case ROBOT_TYPE:
+			this.gameModelController.mqttAddRobot(key, message);
+			break;
+
+		case ROBOT_POSITION:
+		case ROBOT_SETPOSITION:
+			this.robotModelController.mqttRobotPosition(key, message, mqtttopic == MqttTopic.ROBOT_SETPOSITION);
+			break;
+
+		case ROBOT_PROGRESS:
+			this.robotModelController.mqttRobotProgress(key, message);
+
+		case ROBOT_SCORE:
+			this.robotModelController.mqttScoreUpdate(key, message);
+
+		case CONTROL_VIRTUALSPEED:
+			this.gameModelController.mqttSetRobotVirtualspeed(Float.parseFloat(message));
+			break;
+
+		case CONTROL_HESITATIONTIME:
+			this.gameModelController.mqttSetRobotHesitationTime(message);
+
+		case MAP_WALLS:
+			this.stageModelController.mqttSetWalls(message);
+			break;
+
+		case MAP_FOOD:
+			this.stageModelController.mqttSetFood(message);
+			break;
+
+		case FIELD_FOOD:
+			this.stageModelController.mqttSetFieldFood(key, message);
+			break;
+
+		case CONTROL_STATE:
+			this.gameModelController.mqttSetGameState(message);
+			break;
+
+		case EVENT_ERROR_ROBOT_CONNECTION:
+		case EVENT_ERROR_ROBOT_ROBOTICS:
+			IRobotWriteable r = this.game.getRobotsWriteable().get(key);
+			r.setErrorState(true);
+			r.emitEvent(UpdateType.ROBOT_STATE);
+			break;
+
+		case SETTINGS_VISU_REQUEST:
+			if (this.visualization != null)
+				this.sendMqttMessage(MqttTopic.SETTINGS_VISU_RESPONSE, null, this.visualization.getSettings());
+			break;
+
+		case SETTINGS_VISU_SET:
+			if (this.visualization != null)
+				this.visualization.setSettings(message);
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+	@Override
+	public void registerVisualization(IVisualization visualization) {
+		this.visualization = visualization;
 	}
 
 }
