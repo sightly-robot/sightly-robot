545cffa1ff268c92d6b8595f1bbc50a1f9d0f056
Timon Barlag <timon.barlag@gmx.de>
2015-11-30 16:41:20 +0000

    AI: changed test folder
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@334 8fccc7c9-0077-40ea-a901-f51b848cf750

---
+++ Ai/trunk/Ai/test/de/unihannover/swp2015/robots2/tests/MapToGraphTest.java
@@ -0,0 +1,14 @@
+/*package de.unihannover.swp2015.robots2.tests;
+
+import de.unihannover.swp2015.robots2.model.implementation.Stage;
+import junit.framework.TestCase;
+
+public class MapToGraphTest extends TestCase {
+	protected Stage stage;
+
+	protected void setUp() {
+		this.stage = new Stage();
+	}
+
+}
+*/
