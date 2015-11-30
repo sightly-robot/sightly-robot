b7a985fca97055ad8994a135621961f4a5653b67
Lenard Spiecker <lenardspiecker@live.de>
2015-11-30 14:08:48 +0000

    Robot: Progressupdates now interpolated and measured.
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@330 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Robot/trunk/AbstractRobot/src/de/unihannover/swp2015/robots2/abstractrobot/automate/AbstractAutomate.java
+++ Robot/trunk/AbstractRobot/src/de/unihannover/swp2015/robots2/abstractrobot/automate/AbstractAutomate.java
@@ -9,114 +9,126 @@ import de.unihannover.swp2015.robots2.model.interfaces.IPosition.Orientation;
 import de.unihannover.swp2015.robots2.model.interfaces.IRobot;
 
 /**
- * AbstractAutomate is a Runnable Class, that automatically creates 
- * an Thread for controlling the current State of a Hard- or SoftwareRoboter.
+ * AbstractAutomate is a Runnable Class, that automatically creates an Thread
+ * for controlling the current State of a Hard- or SoftwareRoboter.
  * 
  * @author Lenard Spiecker
  */
 public abstract class AbstractAutomate implements AiEventObserver, Runnable {
 
-		// Model:
-		protected IRobotController robotController;
-		protected IRobot robot;
+	// Model:
+	protected IRobotController robotController;
+	protected IRobot robot;
 
-		// Thread:
-		private Thread automation = new Thread(this);
-		private static final long LOOP_WAIT_MS = 30;
+	// Thread:
+	private Thread automation = new Thread(this);
+	private static final long LOOP_WAIT_MS = 30;
 
-		// State:
-		private IState state;
+	// State:
+	private IState state;
 
-		// Positioning:
-		private Point nextPosition = new Point(0, 0);
-		
-		//Progress:
-		private static final long PROGRESS_UPDATE_DURATION = 100;
-		private long nextUpdateTime;
-		
-		
+	// Positioning:
+	private Point nextPosition = new Point(0, 0);
 
-		public AbstractAutomate(IRobotController robotController, IState initialState) {
-			this.robotController = robotController;
-			robot = robotController.getMyself();
+	// Progress:
+	private static final long PROGRESS_UPDATE_DURATION = 100;
+	private long nextUpdateTime;
 
-			state = initialState;
-		}
-		
-		public void start()
-		{
-			automation.start();
-		}
+	private double[] progressMeasurements = new double[] { 1000, 1000, 1000, 1000 };
+	private Direction currentDirection;
+	private long lastWaitTime;
 
-		@Override
-		public void run() {
-			IState tempState = state;
-			while (!Thread.interrupted()) {
-				tempState = state.execute();
+	public AbstractAutomate(IRobotController robotController, IState initialState) {
+		this.robotController = robotController;
+		robot = robotController.getMyself();
 
-				if (state != tempState) {
-					if (tempState.isWait()) {
-						// Update Only Position:
-						robotController.updatePosition(nextPosition.x, nextPosition.y,
-								robot.getPosition().getOrientation());
-					}
-					state = tempState;
-					state.start();
-					//System.out.println(state.name());
+		state = initialState;
+	}
+
+	public void start() {
+		state.start();
+		automation.start();
+	}
+
+	@Override
+	public void run() {
+		IState tempState = state;
+		while (!Thread.interrupted()) {
+			tempState = state.execute();
+
+			if (state != tempState) {
+				if (tempState.isWait()) {
+					// Update Only Position:
+					robotController.updatePosition(nextPosition.x, nextPosition.y,
+							robot.getPosition().getOrientation());
+					// Measurements:
+					progressMeasurements[currentDirection.ordinal()] = System.currentTimeMillis() - lastWaitTime;
 				}
+				state = tempState;
+				state.start();
+				// System.out.println(state.name());
+			}
+
+			if (System.currentTimeMillis() > nextUpdateTime) {
 				
-				if(System.currentTimeMillis() > nextUpdateTime)
-				{
-					robotController.updatePositionProgress((int) (state.getProgress()*1000));
-					nextUpdateTime = System.currentTimeMillis() + PROGRESS_UPDATE_DURATION;
-				}
+				robotController
+						.updatePositionProgress((int) ((Math.min(progressMeasurements[currentDirection.ordinal()],
+								(System.currentTimeMillis() - lastWaitTime))
+						/ progressMeasurements[currentDirection.ordinal()]) * 1000));
+				nextUpdateTime = System.currentTimeMillis() + PROGRESS_UPDATE_DURATION;
+			}
 
-				synchronized (automation) {
-					try {
-						automation.wait(LOOP_WAIT_MS);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
+			synchronized (automation) {
+				try {
+					automation.wait(LOOP_WAIT_MS);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
 			}
 		}
+	}
 
-		@Override
-		public boolean nextOrientationEvent(Orientation orientation) {
-			if (state.isWait()) {
-				nextPosition.setLocation(robot.getPosition().getX(), robot.getPosition().getY());
-				switch (orientation) {
-				case NORTH:
-					nextPosition.translate(0, -1);
-					break;
-				case EAST:
-					nextPosition.translate(+1, 0);
-					break;
-				case SOUTH:
-					nextPosition.translate(0, +1);
-					break;
-				case WEST:
-					nextPosition.translate(-1, 0);
-					break;
-				}
-
-				// Set new State:
-				state = state.getStateForDirection(Direction.calcDirection(robot.getPosition().getOrientation(),orientation));
-//				System.out.println(state.name());
-
-				// Update Only Orientation:
-				robotController.updatePosition(robot.getPosition().getX(), robot.getPosition().getY(), orientation);
-				
-				synchronized (automation) {
-					automation.notify();
-				}
-				return true;
+	@Override
+	public boolean nextOrientationEvent(Orientation orientation) {
+		if (state.isWait()) {
+			nextPosition.setLocation(robot.getPosition().getX(), robot.getPosition().getY());
+			switch (orientation) {
+			case NORTH:
+				nextPosition.translate(0, -1);
+				break;
+			case EAST:
+				nextPosition.translate(+1, 0);
+				break;
+			case SOUTH:
+				nextPosition.translate(0, +1);
+				break;
+			case WEST:
+				nextPosition.translate(-1, 0);
+				break;
 			}
-			return false;
-		}
 
-		@Override
-		public void nextButOneOrientationEvent(Orientation orientation) {
-			// TODO Blinking right or left.
+			// Set new State:
+			currentDirection = Direction.calcDirection(robot.getPosition().getOrientation(), orientation);
+			state = state.getStateForDirection(currentDirection);
+			state.start();
+			// System.out.println(state.name());
+
+			// Update Only Orientation:
+			robotController.updatePosition(robot.getPosition().getX(), robot.getPosition().getY(), orientation);
+
+			// Measurements:
+			lastWaitTime = System.currentTimeMillis();
+
+			synchronized (automation) {
+				automation.notify();
+			}
+			return true;
 		}
+		return false;
+	}
+
+	@Override
+	public void nextButOneOrientationEvent(Orientation orientation) {
+		// TODO Blinking right or left.
+	}
 }
