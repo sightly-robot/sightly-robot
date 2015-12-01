19e7dc538db62fe378d99264e2805c44c1f29a12
Tim Ebbeke <tim06tr@gmail.com>
2015-12-01 02:14:49 +0000

    Added InputDialog for Strings.
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@359 8fccc7c9-0077-40ea-a901-f51b848cf750

---
+++ Application/trunk/Application/src/de/unihannover/swp2015/robots2/dialogs/DialogFactory.java
@@ -0,0 +1,25 @@
+package de.unihannover.swp2015.robots2.dialogs;
+
+import java.io.IOException;
+
+import org.apache.pivot.beans.BXMLSerializer;
+import org.apache.pivot.serialization.SerializationException;
+import org.apache.pivot.wtk.DialogCloseListener;
+import org.apache.pivot.wtk.Window;
+
+public class DialogFactory {
+	public static InputDialog createInputDialog(Window owner, DialogCloseListener closeListener) {
+        BXMLSerializer bxmlSerializer = new BXMLSerializer();
+        InputDialog dialog = null;
+		try {
+			dialog = (InputDialog)bxmlSerializer.readObject(DialogFactory.class.getResource("/de/unihannover/swp2015/robots2/InputDialog.bxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SerializationException e) {
+			e.printStackTrace();
+		}
+		dialog.setModal(true);
+		dialog.open(owner, closeListener);
+		return dialog;
+	}
+}
---
+++ Application/trunk/Application/src/de/unihannover/swp2015/robots2/dialogs/InputDialog.java
@@ -0,0 +1,23 @@
+package de.unihannover.swp2015.robots2.dialogs;
+
+import java.net.URL;
+
+import org.apache.pivot.beans.BXML;
+import org.apache.pivot.beans.Bindable;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.Dialog;
+import org.apache.pivot.wtk.TextArea;
+
+public class InputDialog extends Dialog implements Bindable {
+	@BXML private TextArea textBox; 
+	
+	public String getText() {
+		return textBox.getText();
+	}
+
+	@Override
+	public void initialize(Map<String, Object> arg0, URL arg1, Resources arg2) {
+		// TODO Auto-generated method stub		
+	}
+}
