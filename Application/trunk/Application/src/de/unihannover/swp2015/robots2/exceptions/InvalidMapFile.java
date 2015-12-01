1b4607db0d80cb8d372c788d6b42182583e47022
Tim Ebbeke <tim06tr@gmail.com>
2015-12-01 00:14:22 +0000

    Moved exception to extra package.
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@354 8fccc7c9-0077-40ea-a901-f51b848cf750

---
+++ Application/trunk/Application/src/de/unihannover/swp2015/robots2/exceptions/InvalidMapFile.java
@@ -0,0 +1,20 @@
+package de.unihannover.swp2015.robots2.exceptions;
+
+/**
+ * This exception is throw whenever the map file contains inconsistent
+ * data or data that makes no sense.
+ * This is exception is NOT thrown, when the json format is invalid.
+ *
+ * @author Tim
+ */
+public class InvalidMapFile extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public InvalidMapFile(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public InvalidMapFile(String message) {
+		super(message);
+	}
+}
