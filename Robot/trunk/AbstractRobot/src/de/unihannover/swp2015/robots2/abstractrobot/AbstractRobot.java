174570ef15d6ab58ab677cb55d2482f8459dea7f
Lenard Spiecker <lenardspiecker@live.de>
2015-12-01 15:49:56 +0000

    Robot: Working with blink
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@388 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Robot/trunk/AbstractRobot/src/de/unihannover/swp2015/robots2/abstractrobot/AbstractRobot.java
+++ Robot/trunk/AbstractRobot/src/de/unihannover/swp2015/robots2/abstractrobot/AbstractRobot.java
@@ -5,6 +5,7 @@ import de.unihannover.swp2015.robots2.aiinterface.AbstractAi;
 import de.unihannover.swp2015.robots2.controller.interfaces.IRobotController;
 import de.unihannover.swp2015.robots2.controller.main.RobotMainController;
 import de.unihannover.swp2015.robots2.core.AI;
+import de.unihannover.swp2015.robots2.model.interfaces.IEvent;
 import de.unihannover.swp2015.robots2.model.interfaces.IPosition.Orientation;
 
 /**
@@ -29,15 +30,19 @@ public abstract class AbstractRobot {
 	 * Initializes the AbstractRobot instance by initializing the robot
 	 * controller and AI.
 	 */
-	public AbstractRobot() {
-		// TODO Init IRobotController:
-		// robotController = new
-		// TestRobotController(true,2,2,Orientation.NORTH);
-		robotController = new RobotMainController(true);
-		robotController.updatePosition(2, 2, Orientation.NORTH);
+	public AbstractRobot(boolean isHardware) {
+
+		robotController = new RobotMainController(isHardware);
+		while(!robotController.startMqtt("tcp://192.168.1.66"))
+		{
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 
 		// TODO Init AbstractAI
-		// ai = new TestAi(robotController);
 		ai = new AI(robotController);
 		ai.setRelativeSpeed(1, 1, 1);
 
