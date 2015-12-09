806edec6f7a09f8a4d888e0b979ab470f00d99ee
Michael Thies <mail@mhthies.de>
2015-12-09 20:22:41 +0000

    Change Abstract Model: Store observers in set and provide unobserve() method
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@482 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Protocol/trunk/Protocol/src/de/unihannover/swp2015/robots2/model/implementation/AbstractModel.java
+++ Protocol/trunk/Protocol/src/de/unihannover/swp2015/robots2/model/implementation/AbstractModel.java
@@ -1,7 +1,9 @@
 package de.unihannover.swp2015.robots2.model.implementation;
 
-import java.util.Vector;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import de.unihannover.swp2015.robots2.model.externalInterfaces.IModelObserver;
 import de.unihannover.swp2015.robots2.model.interfaces.IAbstractModel;
@@ -19,17 +21,30 @@ import de.unihannover.swp2015.robots2.model.writeableInterfaces.IAbstractModelWr
  * @author Patrick Kawczynski
  * @author Michael Thies
  */
-public abstract class AbstractModel implements IAbstractModel, IAbstractModelWriteable {
+public abstract class AbstractModel implements IAbstractModel,
+		IAbstractModelWriteable {
 	/** List of observers of this model object. */
-	private List<IModelObserver> observers;
+	private final Set<IModelObserver> observers;
+	/** Lock Object to prevent change of observer list while iterating */
+	private final ReadWriteLock observerlock;
 
 	public AbstractModel() {
-		this.observers = new Vector<IModelObserver>();
+		this.observers = new HashSet<IModelObserver>();
+		this.observerlock = new ReentrantReadWriteLock();
 	}
 
 	@Override
 	public void observe(IModelObserver observer) {
+		observerlock.writeLock().lock();
 		this.observers.add(observer);
+		observerlock.writeLock().unlock();
+	}
+
+	@Override
+	public void unobserve(IModelObserver observer) {
+		observerlock.writeLock().lock();
+		this.observers.remove(observer);
+		observerlock.writeLock().unlock();
 	}
 
 	@Override
@@ -40,7 +55,9 @@ public abstract class AbstractModel implements IAbstractModel, IAbstractModelWri
 	@Override
 	public void emitEvent(Event.UpdateType type, Object object) {
 		IEvent event = new Event(type, object);
+		observerlock.readLock().lock();
 		for (IModelObserver o : this.observers)
 			o.onModelUpdate(event);
+		observerlock.readLock().unlock();		
 	}
 }
