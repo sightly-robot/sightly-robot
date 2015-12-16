b7c56fa91916c1789d45cd4773836b7bf51efe7b
Lenard Spiecker <lenardspiecker@live.de>
2015-12-16 16:16:58 +0000

    Robot: Abstractrobot bug fix and debug info
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@540 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Robot/trunk/SoftwareRobot/src/de/unihannover/swp2015/robots2/robot/softwarerobot/Main.java
+++ Robot/trunk/SoftwareRobot/src/de/unihannover/swp2015/robots2/robot/softwarerobot/Main.java
@@ -8,6 +8,7 @@ package de.unihannover.swp2015.robots2.robot.softwarerobot;
 public class Main {
 
 	public static void main(String[] args) {
-		new SoftwareRobot(args.length>1?args[1]:null);
+		System.out.println("Found Argument: "+args[0]);
+		new SoftwareRobot(args.length>0?args[0]:null);
 	}
 }
