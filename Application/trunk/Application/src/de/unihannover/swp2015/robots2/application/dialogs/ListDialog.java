1fc2fe246f20d29e7732b20782e430b64e631540
Tim Ebbeke <tim06tr@gmail.com>
2016-01-13 16:56:47 +0000

    Added OK button to list dialog.
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@614 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Application/trunk/Application/src/de/unihannover/swp2015/robots2/application/dialogs/ListDialog.java
+++ Application/trunk/Application/src/de/unihannover/swp2015/robots2/application/dialogs/ListDialog.java
@@ -7,8 +7,11 @@ import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.List;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.Button;
+import org.apache.pivot.wtk.ButtonPressListener;
 import org.apache.pivot.wtk.Dialog;
 import org.apache.pivot.wtk.ListView;
+import org.apache.pivot.wtk.PushButton;
 
 /**
  * Dialog class for choosing elements from a table
@@ -17,14 +20,24 @@ import org.apache.pivot.wtk.ListView;
 public class ListDialog extends Dialog implements Bindable {
 
 	private @BXML ListView list;
+	private @BXML PushButton okButton;
+	
 	private List <String> elements;
 	
 	/**
 	 * BXML Bindable
 	 */
 	@Override
-	public void initialize(Map<String, Object> arg0, URL arg1, Resources arg2) {		
+	public void initialize(Map<String, Object> arg0, URL arg1, Resources arg2) {
+		okButton.getButtonPressListeners().add(closeAction);
 	}
+	
+	private ButtonPressListener closeAction = new ButtonPressListener() {
+		@Override
+		public void buttonPressed(Button button) {
+			close();
+		}
+	};
 	
 	public void setListElements(List <String> elements) {
 		this.elements = elements; 
