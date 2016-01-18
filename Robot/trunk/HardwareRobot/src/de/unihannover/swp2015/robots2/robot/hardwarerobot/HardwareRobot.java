4989b10ebc596f8afb077732d2867b02b8234f53
Lenard Spiecker <lenardspiecker@live.de>
2016-01-18 19:32:08 +0000

    Robot: Fromatting and Log4j bug fix
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@664 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Robot/trunk/HardwareRobot/src/de/unihannover/swp2015/robots2/robot/hardwarerobot/HardwareRobot.java
+++ Robot/trunk/HardwareRobot/src/de/unihannover/swp2015/robots2/robot/hardwarerobot/HardwareRobot.java
@@ -29,29 +29,30 @@ public class HardwareRobot extends AbstractRobot implements IHardwareRobot {
 
 	/** LOGGER: */
 	private static Logger LOGGER = LogManager.getLogger(HardwareRobot.class.getName());
+
 	/**
 	 * Initializes the sensor controllers, automate and AI.
 	 */
 	public HardwareRobot(String brokerIP) {
-		super(true,brokerIP);
+		super(true, brokerIP);
 		// PreInitialize Controller Instances:
-		BlinkLEDAndServoController.getInstance().setAccentColor(robotController.getMyself().getColor());;
+		BlinkLEDAndServoController.getInstance().setAccentColor(robotController.getMyself().getColor());
+		;
 		Pi2GoGPIOController.getInstance();
 		MotorController.getInstance();
 		// SoundController.getInstance();
 		// ColorSensorController.getInstance();
 		// CompassController.getInstance();
-		
+
 		automate = new HardwareAutomate(robotController);
 		automate.start();
-		robotController.registerHardwareRobot((IHardwareRobot)automate);
+		robotController.registerHardwareRobot((IHardwareRobot) automate);
 		ai.setAiEventObserver(automate);
-		
+
 		initializeGPIOs();
 
 		blink(robotController.getMyself().getColor());
 	}
-	
 
 	@Override
 	public void setSettings(String settings) {
@@ -59,26 +60,25 @@ public class HardwareRobot extends AbstractRobot implements IHardwareRobot {
 			String setting[] = settings.split("=");
 			switch (setting[0]) {
 			case "dir":
-				//TODO use own AI with eventhandling...
+				// TODO use own AI with eventhandling...
 				if (!automate.nextOrientationEvent(Orientation.valueOf(setting[1]))) {
 					automate.nextButOneOrientationEvent(Orientation.valueOf(setting[1]));
 				}
 				break;
 			case "ai":
-				if(!Boolean.parseBoolean(setting[1]))
-				{
-					ai.setAiEventObserver(new AiEventObserver() {			
+				if (!Boolean.parseBoolean(setting[1])) {
+					ai.setAiEventObserver(new AiEventObserver() {
 						@Override
 						public boolean nextOrientationEvent(Orientation orientation) {
 							return false;
 						}
+
 						@Override
 						public void nextButOneOrientationEvent(Orientation orientation) {
 						}
 					});
-					//TODO load own AI, controlled via Settings
-				}else
-				{
+					// TODO load own AI, controlled via Settings
+				} else {
 					ai.setAiEventObserver(automate);
 				}
 				break;
@@ -102,48 +102,38 @@ public class HardwareRobot extends AbstractRobot implements IHardwareRobot {
 		if (LEDAndServoController.getInstance() instanceof BlinkLEDAndServoController) {
 			((BlinkLEDAndServoController) LEDAndServoController.getInstance()).startBlinking(color);
 			LOGGER.info("I'm blinking!");
-		}else
-		{
+		} else {
 			LOGGER.warn("Wrong LEDAndServoController! Blink not possible!");
 		}
 	}
-	
+
 	/**
 	 * Initializes GPIO Event-handling.
 	 */
-	private void initializeGPIOs()
-	{
+	private void initializeGPIOs() {
 		Pi2GoGPIOController.getInstance().getButton().addListener(new GpioPinListenerDigital() {
 			@Override
 			public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-				if(event.getState().isHigh())
-				{
+				if (event.getState().isHigh()) {
 					switch (robotController.getMyself().getState()) {
 					case SETUPSTATE:
-						System.out.println("Robot ready!");
+						LOGGER.info("Robot ready!");
 						robotController.setRobotReady();
 						break;
 					case ENABLED:
-						System.out.println("Robot disable!");
+						LOGGER.info("Robot disable!");
 						robotController.disableMyself();
-						break;	
+						break;
 					default:
-						System.out.println("Robot delete!");
+						LOGGER.info("Robot delete!");
 						robotController.deleteMyself();
 						break;
 					}
-					
+
 				}
 			}
 		});
-		GpioPinListenerDigital gChangeEvent = new GpioPinListenerDigital() {
-			@Override
-			public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-				((HardwareAutomate)automate).getAutomation().notify();
-			}
-		};
-		Pi2GoGPIOController.getInstance().getLineLeft().addListener(gChangeEvent);
-		Pi2GoGPIOController.getInstance().getLineRight().addListener(gChangeEvent);
+		Pi2GoGPIOController.getInstance().getLineLeft().addListener(((HardwareAutomate) automate));
+		Pi2GoGPIOController.getInstance().getLineRight().addListener(((HardwareAutomate) automate));
 	}
 }
-
