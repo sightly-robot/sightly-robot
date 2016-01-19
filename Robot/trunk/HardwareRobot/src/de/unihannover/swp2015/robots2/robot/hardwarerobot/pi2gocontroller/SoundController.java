0d0b03d4d571263f354e362dcceccc45172ff79c
Lenard Spiecker <lenardspiecker@live.de>
2016-01-19 22:59:25 +0000

    Robot: Sound and LED Update
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@696 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Robot/trunk/HardwareRobot/src/de/unihannover/swp2015/robots2/robot/hardwarerobot/pi2gocontroller/SoundController.java
+++ Robot/trunk/HardwareRobot/src/de/unihannover/swp2015/robots2/robot/hardwarerobot/pi2gocontroller/SoundController.java
@@ -2,7 +2,13 @@ package de.unihannover.swp2015.robots2.robot.hardwarerobot.pi2gocontroller;
 
 import java.applet.Applet;
 import java.applet.AudioClip;
+import java.io.IOException;
 import java.net.URL;
+
+import de.unihannover.swp2015.robots2.controller.interfaces.IRobotController;
+import de.unihannover.swp2015.robots2.model.externalInterfaces.IModelObserver;
+import de.unihannover.swp2015.robots2.model.interfaces.IEvent;
+import de.unihannover.swp2015.robots2.robot.abstractrobot.automate.AbstractAutomate;
 
 /**
  * Controller for hard-coded sound effects.<br>
@@ -11,13 +17,15 @@ import java.net.URL;
  * 
  * @author Lenard Spiecker
  */
-public class SoundController {
+public class SoundController implements IModelObserver {
 
 	/** The Singleton instance of the SoundController. */
 	private static SoundController instance;
 
 	private AudioClip gameMusic;
 	private AudioClip startSound;
+
+	private IRobotController robotController;
 
 	/**
 	 * Initializes the Singleton instance.<br>
@@ -43,6 +51,23 @@ public class SoundController {
 			instance = new SoundController();
 		}
 		return instance;
+	}
+
+	public void initAutoSound(IRobotController robotController) {
+		this.robotController = robotController;
+
+		robotController.getMyself().observe(this);
+		robotController.getGame().observe(this);
+
+		SoundController.getInstance().speak("Hello, My name is " + robotController.getMyself().getName()
+				+ "! Do you like my color? Anyway... I am now online!");
+	}
+
+	public void speak(String message) {
+		try {
+			Runtime.getRuntime().exec("echo '" + message + "' | festival --tts");
+		} catch (IOException e) {
+		}
 	}
 
 	/**
@@ -79,6 +104,46 @@ public class SoundController {
 	 */
 	public void stopMusic() {
 		gameMusic.stop();
+	}
+
+	@Override
+	public void onModelUpdate(IEvent event) {
+		switch (event.getType()) {
+		case ROBOT_STATE:
+			switch (robotController.getMyself().getState()) {
+			case CONNECTED:
+				speak("I'm connected now!");
+				break;
+			case SETUPSTATE:
+				speak("Take me to my place! Then click on my Button!");
+				break;
+			case ENABLED:
+				speak("I'm ready now!");
+				break;
+			case ROBOTICS_ERROR:
+				speak("An Error occured!");
+				break;
+			case MANUAL_DISABLED_GUI:
+				speak("Why do you disable me?");
+				break;
+			case MANUAL_DISABLED_ROBOT:
+				speak("Outch!");
+				break;
+			case DISCONNECTED:
+				speak("Does anybody know, where the Broker is?");
+				break;
+			}
+			break;
+		case GAME_STATE:
+			speak("Let's go!");
+			break;
+		case ROBOT_DELETE:
+			speak("Good Bye, Sir!");
+			break;
+		default:
+			break;
+		}
+
 	}
 
 }
