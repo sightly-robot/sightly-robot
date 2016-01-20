71221bd75fd48a2c663830ada9d34c193d701758
Tim Ebbeke <tim06tr@gmail.com>
2016-01-20 04:16:58 +0000

    Added getter and setter for id.
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@707 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Utils/trunk/Utils/src/de/unihannover/swp2015/robots2/utils/VisualizationOptions.java
+++ Utils/trunk/Utils/src/de/unihannover/swp2015/robots2/utils/VisualizationOptions.java
@@ -240,6 +240,14 @@ public class VisualizationOptions {
 			this.renderVirtualRobots = options.renderVirtualRobots;
 	}
 	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 	public Optional<String> getCycleTexturePack() {
 		return cycleTexturePack;
 	}
