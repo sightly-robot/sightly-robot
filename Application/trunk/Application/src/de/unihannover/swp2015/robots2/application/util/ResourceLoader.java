41ecfc277e87c8d4174af28a8f1bc0a40a93972e
Patrick Kawczynski <kaw.patrick@yahoo.de>
2016-01-23 13:05:53 +0000

    Fix FilesystemNotFoundExcpion of jar-file
    git-svn-id: https://dev.se.uni-hannover.de/svn/shared/LV/swp1516/roboter-labyrinth2@782 8fccc7c9-0077-40ea-a901-f51b848cf750

--- Application/trunk/Application/src/de/unihannover/swp2015/robots2/application/util/ResourceLoader.java
+++ Application/trunk/Application/src/de/unihannover/swp2015/robots2/application/util/ResourceLoader.java
@@ -1,10 +1,17 @@
 package de.unihannover.swp2015.robots2.application.util;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResourceLoader {
 	/**
@@ -16,7 +23,16 @@ public class ResourceLoader {
 	 * @throws URISyntaxException Thrown when resource path is not a valid path.
 	 */
 	public static String loadResourceAsString(String path) throws IOException, URISyntaxException {
-		return new String(Files.readAllBytes(Paths.get(ResourceLoader.class.getResource(path).toURI())));
+		BufferedReader bf = new BufferedReader( new InputStreamReader(ResourceLoader.class.getResourceAsStream(path)));
+		
+		String res = "";
+		String line = "";
+		
+		while( (line = bf.readLine()) != null ) {
+			res += line;
+		}
+	
+		return res;
 	}
 	
 	/**
